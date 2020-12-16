60c367599526283b87d2b2117b90cb35cd007897
Mateusz Jałocha <38622419+MateuszJalocha@users.noreply.github.com>
2020-12-16 18:42:41 +0100

    Script to create tables

--- Database_scripts/db_tables.py
+++ Database_scripts/db_tables.py
@@ -5,24 +5,17 @@ import pyodbc
 import numpy as np
 import pandas as pd
 
-def create_table(conn, query):
-    cursor = conn.cursor()
-    cursor.execute(query)
-    cursor.close()
+def create_table(engine, query):
+    conn = engine.connect()
+    conn.execute(query)
+    conn.close()
+
 
 #Database connection
-server = 'tcp:apartmentsproject.database.windows.net'
-database = 'apartments'
-username = 'apartments'
-password = 'Pingwin9815$'   
-driver= '{ODBC Driver 13 for SQL Server}'
+config = configparser.ConfigParser() 
+config.read('Mieszkania/Database_scripts/config.ini')
 
-databaseConnection = pyodbc.connect('DRIVER='+driver+';\
-                    SERVER='+server+';\
-                    PORT=1433;\
-                    DATABASE='+database+';\
-                    UID='+username+';\
-                    PWD='+ password)
+engine = connect_database(config, "DATABASE")
 
 #Create tables
 
@@ -30,4 +23,4 @@ create_activeLinks = "CREATE TABLE Active_links (LinkID INT IDENTITY PRIMARY KEY
                                            PageName NVARCHAR (128) NOT NULL,\
                                            Link NVARCHAR(256) NOT NULL)"
 
-create_table(databaseConnection, create_activeLinks)
+create_table(engine, create_activeLinks)
