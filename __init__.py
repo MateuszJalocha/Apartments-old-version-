c12df16193aae905618e337f2842b8fc9d2b77b8
mateusz.jalocha2107@gmail.com <Pingwin123>
2021-04-09 16:37:43 +0200

    run project

--- __init__.py
+++ __init__.py
@@ -1,15 +1,40 @@
-from MorizonScraper import ScrapingMorizon
-from Database_scripts.db_manipulation import DatabaseManipulation
+# Add path to scraping scripts
+import sys
+sys.path.append('Scraping')
+sys.path.append('Database_scripts')
+
+from morizonScraper import ScrapingMorizon
+from otodomScraper import ScrapingOtodom
+from db_manipulation import DatabaseManipulation
 import configparser
 import urllib
 from sqlalchemy import create_engine
 
 if __name__ == "__main__":
-    morizon_scraper = ScrapingMorizon(page = 'https://www.morizon.pl/do-wynajecia/mieszkania',page_name = 'https://www.morizon.pl',max_threads = 30)
-
     # Database connection
     config = configparser.ConfigParser()
     config.read('Database_scripts/config.ini')
+
+    database_manipulation = DatabaseManipulation(config = config, config_database = "DATABASE", table_name_links = "active_links",
+                                                 table_name_offers = "preprocessing_offers", split_size = 1000)
+
+    #Morizon
+    #morizon_scraper = ScrapingMorizon(page = 'https://www.morizon.pl/do-wynajecia/mieszkania',page_name = 'https://www.morizon.pl',max_threads = 30)
+
+    #Otodom
+    otodom_scraper = ScrapingOtodom(page='https://www.otodom.pl/wynajem/mieszkanie/', page_name='https://www.otodom.pl', max_threads=30)
+    otodom_offers = otodom_scraper.get_offers()
+    to_scrape = database_manipulation.push_to_database_links(activeLinks = otodom_offers, page_name = "Otodom",
+                                                 insert_columns = ["pageName", "link"])
+    scraped = otodom_offers.get_details(offers=otodom_offers,split_size=500)
+
+    database_manipulation.push_to_database_offers(offers=scraped,insert_columns=["area", "description", "latitude","longitude",
+                                                                                 "link", "price", "currency","rooms", "floors_number",
+                                                                                 "floor", "type_building", "material_building",
+                                                                                 "year", "headers", "additional_info", "city",
+                                                                                 "address", "district", "voivodeship", "active",
+                                                                                 "scrape_date", "inactive_date", "pageName"])
+
 
     database = "DATABASE"
     params = urllib.parse.quote_plus(
