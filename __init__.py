392f9a0ddb11a11d638211232ef755b844a80199
mateusz.jalocha2107@gmail.com <Pingwin123>
2021-04-26 22:30:56 +0200

    finished for otodom and morizon

--- __init__.py
+++ __init__.py
@@ -4,6 +4,15 @@ sys.path.append('Scraping')
 sys.path.append('Database_scripts')
 sys.path.append('Preprocessing scripts')
 
+#Colab paths
+sys.path.append('/content/Apartments')
+sys.path.append('/content/Apartments/Scraping')
+sys.path.append('/content/Apartments/Database_scripts')
+sys.path.append('/content/Apartments/Preprocessing_scripts')
+sys.path.append('/Apartments/Scraping')
+sys.path.append('/Apartments/Database_scripts')
+sys.path.append('/Apartments/Preprocessing_scripts')
+
 from morizonScraper import ScrapingMorizon
 from otodomScraper import ScrapingOtodom
 from db_manipulation import DatabaseManipulation
